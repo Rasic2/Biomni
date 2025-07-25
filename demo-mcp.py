a9170665ef0a4c683e3d89a2201aea2a47b3300d
Rasic2 <1051987201@qq.com>
2025-07-25 16:16:09 +0800

    feat: add superior

--- demo-mcp.py
+++ demo-mcp.py
@@ -1,10 +1,8 @@
 from dotenv import load_dotenv, find_dotenv
 from langchain.chat_models import init_chat_model
 from langchain_mcp_adapters.client import MultiServerMCPClient
-from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.graph import StateGraph, MessagesState, START, END
 from langgraph.prebuilt import ToolNode
-from langgraph.prebuilt import create_react_agent
 from langgraph_supervisor import create_supervisor
 
 # 初始化环境变量
@@ -13,97 +11,104 @@ load_dotenv(find_dotenv())
 # Initialize the model
 model = init_chat_model("azure_openai:gpt-4o")
 
+# Initialize MCP client
+mcp_server_toolset = MultiServerMCPClient(
+    {
+        "dpa": {
+            "url": "http://eyjo1360201.bohrium.tech:50001/sse",
+            "transport": "sse",
+        },
+        "smiles": {
+            "url": "http://ctoe1357111.bohrium.tech:50002/sse",
+            "transport": "sse"
+        }
+    }
+)
 
-async def make_dpa_graph():
-    # Set up MCP client
-    client = MultiServerMCPClient(
+
+async def get_tools(server_name):
+    return await mcp_server_toolset.get_tools(server_name=server_name)
+
+
+async def dpa_llm_node(state: MessagesState):
+    messages = state["messages"]
+    tools = await get_tools(server_name="dpa")
+    model_with_tools = model.bind_tools(tools)
+    response = await model_with_tools.ainvoke(messages)
+    return {"messages": [response]}
+
+
+async def dpa_tool_node(state: MessagesState):
+    tools = await get_tools(server_name="dpa")
+    return ToolNode(tools)
+
+
+def should_continue(state: MessagesState):
+    messages = state["messages"]
+    last_message = messages[-1]
+    if hasattr(last_message, 'tool_calls') and last_message.tool_calls:
+        return "tool_node"
+    return END
+
+
+dpa_agent_graph = (
+    StateGraph(MessagesState)
+    .add_node("dpa_llm_node", dpa_llm_node)
+    .add_node("dpa_tool_node", dpa_tool_node)
+    .add_edge(START, "dpa_llm_node")
+    .add_conditional_edges(
+        "dpa_llm_node",
+        should_continue,
         {
-            "dpa": {
-                "url": "http://eyjo1360201.bohrium.tech:50001/sse",
-                "transport": "sse",
-            }
+            "tool_node": "dpa_tool_node",
+            END: END
         }
     )
-    tools = await client.get_tools()
-    dpa_agent = create_react_agent(
-        model=model,
-        tools=tools,
-        name="dpa_expert",
-        prompt="You are a dpa expert. Always use one tool at a time."
+    .add_edge("dpa_tool_node", "dpa_llm_node")
+    .compile(name="dpa_agent")
+)
+
+
+async def smiles_llm_node(state: MessagesState):
+    messages = state["messages"]
+    tools = await get_tools(server_name="smiles")
+    model_with_tools = model.bind_tools(tools)
+    response = await model_with_tools.ainvoke(messages)
+    return {"messages": [response]}
+
+
+async def smiles_tool_node(state: MessagesState):
+    tools = await get_tools(server_name="smiles")
+    return ToolNode(tools)
+
+
+smiles_agent_graph = (
+    StateGraph(MessagesState)
+    .add_node("smiles_llm_node", dpa_llm_node)
+    .add_node("smiles_tool_node", smiles_tool_node)
+    .add_edge(START, "smiles_llm_node")
+    .add_conditional_edges(
+        "smiles_llm_node",
+        should_continue,
+        {
+            "tool_node": "smiles_tool_node",
+            END: END
+        }
     )
-    return dpa_agent
+    .add_edge("smiles_tool_node", "smiles_llm_node")
+    .compile(name="smiles_agent")
+)
 
+supervisor_agent_graph = create_supervisor(
+    agents=[dpa_agent_graph, smiles_agent_graph],
+    model=model,
+).compile()
 
-# async with client.session("dpa") as session:
-#     tools = await load_mcp_tools(session)
-#
-#
-# async def get_tools():
-#     return await client.get_tools()
-
-
-# # Define call_model function
-# async def call_model(state: MessagesState):
-#     messages = state["messages"]
-#     model_with_tools = model.bind_tools(await get_tools())
-#     response = await model_with_tools.ainvoke(messages)
-#     return {"messages": [response]}
-#
-#
-# async def tool_node(state: MessagesState):
-#     # return ToolNode(await get_tools())
-#     return await get_tools()
-
-
-#
-#
-# def should_continue(state: MessagesState):
-#     messages = state["messages"]
-#     last_message = messages[-1]
-#     if last_message.tool_calls:
-#         return "tools"
-#     return END
-#
-#
-# # Define the graph
-# graph = (
-#     StateGraph(MessagesState)
-#     .add_node("call_model", call_model)
-#     .add_node("tools", tool_node)
-#     .add_edge(START, "call_model")
-#     .add_conditional_edges("call_model", should_continue)
-#     .add_edge("tools", "call_model")
-#     .compile(name="New Graph")
-# )
-
-# dpa_agent = create_react_agent(
-#     model=model,
-#     tools=[tool_node],
-#     name="dpa_expert",
-#     prompt="You are a dpa expert. Always use one tool at a time."
-# )
-
-async def app():
-    dpa_agent = await make_dpa_graph()
-
-    # Create supervisor workflow
-    workflow = create_supervisor(
-        [dpa_agent],
-        model=model,
-        prompt=(
-            "You are a team supervisor managing a dpa expert. "
-            "For dpa problems, use dpa_agent."
-        )
-    )
-    # Compile and run
-    app = workflow.compile()
-
-    return app
-# result = app.invoke({
-#     "messages": [
-#         {
-#             "role": "user",
-#             "content": "what's the combined headcount of the FAANG companies in 2024?"
-#         }
-#     ]
-# })
+# Define the main app
+app = (
+    StateGraph(MessagesState)
+    .add_node("superior_agent", supervisor_agent_graph)
+    .add_edge(START, "superior_agent")
+    .add_edge("superior_agent", END)
+    .compile()
+)
